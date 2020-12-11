9aa85a2cb90141a4f346eb45558396d40f1bb99d
xyzuan <i.xyzuan@gmail.com>
2020-12-11 11:08:08 +0000

    overlay-ancient: define device specs.

--- device.mk
+++ device.mk
@@ -30,7 +30,8 @@ $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-ancient
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
