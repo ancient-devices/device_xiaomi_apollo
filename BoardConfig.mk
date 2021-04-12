687384cb199a895416586914feb65a9b047aa70f
goodmeow <harunbam3@gmail.com>
2021-04-12 23:31:36 +0700

    apollo: kernel: import boardconfig
    Change-Id: Ic79681d44f8a7c83213148d6712c64cfc899ece5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,6 +85,20 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
 
 # Kernel
 include device/xiaomi/apollo-kernel/BoardConfigKernel.mk
+TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
+
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_RAMDISK_OFFSET := 0x02000000
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
