07bb3fd3d98359da7611dcb704e92739cb503d3b
goodmeow <goodmeow@nusantaradevs.org>
2021-04-20 12:12:40 +0700

    sm8250-common: build libsdedrm and its dependencies from source
    Change-Id: I5ba23482932f5c434b895b5a415fbf06c63d3fcf
    Signed-off-by: goodmeow <harunbam3@gmail.com>

--- device.mk
+++ device.mk
@@ -168,6 +168,9 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     libdisplayconfig.qti \
     libdisplayconfig.qti.vendor \
+    libsdedrm \
+    libsdmcore \
+    libsdmutils \
     libqdMetaData \
     libtinyxml \
     hwcomposer.kona \
