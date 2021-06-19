77493a248290e9cf2ce596c0a7714e8a837063a8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-19 11:56:54 +0200

    sm8250-common: Build custom QTI vibrator AIDL
    Change-Id: Ica7bd62e1328bd2d3a0278c6ee3901bddb2cb95d

--- device.mk
+++ device.mk
@@ -426,7 +426,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_kona
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
