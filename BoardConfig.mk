6a2ea8b50d4bb2d2f83f790bd1373f817d95012f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-03 11:47:31 +0200

    apollo: Move NFC manifest entries to nfc SKU
    Change-Id: I2d83a4551551f370c1ecffb73502c5bcec0bd5a9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,8 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+ODM_MANIFEST_SKUS += nfc
+ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/manifest_nfc.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollo
