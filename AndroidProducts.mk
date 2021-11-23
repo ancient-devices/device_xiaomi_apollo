7879ce7fe7f50d32dc5cfe223de64881f93b19a2
goodmeow <harunbam3@gmail.com>
2021-11-23 08:27:25 +0100

    ancient: re-initial config

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_apollo.mk
+    $(LOCAL_DIR)/ancient_apollo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_apollo-user \
-    arrow_apollo-userdebug \
-    arrow_apollo-eng
+    ancient_apollo-user \
+    ancient_apollo-userdebug \
+    ancient_apollo-eng
--- ancient_apollo.mk
+++ ancient_apollo.mk
@@ -11,16 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-DEVICE_MAINTAINER := Dobsgw
+TARGET_BOOT_ANIMATION_RES := 1440
+ANCIENT_OFFICIAL ?= true
+ANCIENT_GAPPS ?= true
+TARGET_USES_BLUR := true
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
-PRODUCT_NAME := arrow_apollo
+PRODUCT_NAME := ancient_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
